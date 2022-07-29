801ab355986f7a45468cb6adedb443a1897854fe
Paweł <pawel_gnat@o2.pl>
2022-07-29 20:04:25 +0200

    adding js functions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,50 @@
 const profileCard = document.querySelector('.profile')
-const dailyCurrentWorkTime = document.querySelector('.data-container__bottom-area-daily--current')
-const dailyLastWorkTime = document.querySelector('.data-container__bottom-area-daily--last')
-const weeklyCurrentWorkTime = document.querySelector('.data-container__bottom-area-weekly--current')
-const weeklyLastWorkTime = document.querySelector('.data-container__bottom-area-weekly--last')
-const monthlyCurrentWorkTime = document.querySelector('.data-container__bottom-area-monthly--current')
-const monthlyLastWorkTime = document.querySelector('.data-container__bottom-area-monthly--last')
+const dailyBtn = document.querySelector('#daily_btn')
+const weeklyBtn = document.querySelector('#weekly_btn')
+const monthlyBtn = document.querySelector('#monthly_btn')
+const allTimeBtns = document.querySelectorAll('.timeframe__btn')
+const currentTasks = document.querySelectorAll('.data-container__bottom-area--current')
+const lastTasks = document.querySelectorAll('.data-container__bottom-area--last')
 
 async function worktimeData() {
 	const response = await fetch('./data.json')
 	const data = await response.json()
 
-	dailyCurrentWorkTime.innerText = data[0].timeframes.daily.current + 'hrs'
+	function displayDaily() {
+		for (let i = 0; i < data.length; i++) {
+			currentTasks[i].innerText = data[i].timeframes.daily.current + 'hrs'
 
-	dailyLastWorkTime.innerText = 'Yesterday - ' + data[0].timeframes.daily.previous + 'hrs'
+			lastTasks[i].innerText = 'Yesterday - ' + data[i].timeframes.daily.previous + 'hrs'
+		}
 
-	weeklyCurrentWorkTime.innerText = data[0].timeframes.weekly.current + 'hrs'
+		// dailyBtn.style.opacity = '0'
+		dailyBtn.classList.add('active')
+	}
+	displayDaily()
 
-	weeklyLastWorkTime.innerText = 'Last Week - ' + data[0].timeframes.weekly.previous + 'hrs'
+	dailyBtn.addEventListener('click', e => {
+		for (let i = 0; i < data.length; i++) {
+			currentTasks[i].innerText = data[i].timeframes.daily.current + 'hrs'
 
-	monthlyCurrentWorkTime.innerText = data[0].timeframes.monthly.current + 'hrs'
+			lastTasks[i].innerText = 'Yesterday - ' + data[i].timeframes.daily.previous + 'hrs'
+		}
+	})
 
-	monthlyLastWorkTime.innerText = 'Last Month - ' + data[0].timeframes.monthly.previous + 'hrs'
+	weeklyBtn.addEventListener('click', e => {
+		for (let i = 0; i < data.length; i++) {
+			currentTasks[i].innerText = data[i].timeframes.weekly.current + 'hrs'
+
+			lastTasks[i].innerText = 'Last Week - ' + data[i].timeframes.weekly.previous + 'hrs'
+		}
+	})
+
+	monthlyBtn.addEventListener('click', e => {
+		for (let i = 0; i < data.length; i++) {
+			currentTasks[i].innerText = data[i].timeframes.monthly.current + 'hrs'
+
+			lastTasks[i].innerText = 'Last Month - ' + data[i].timeframes.monthly.previous + 'hrs'
+		}
+	})
 }
 worktimeData()
 
