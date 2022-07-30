411b49ec913ffd9fb2ce08c7c4fbebbf838325f5
Paweł <pawel_gnat@o2.pl>
2022-07-30 15:27:29 +0200

    styling my media queries

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-const profileCard = document.querySelector('.profile')
-const taskCards = document.querySelectorAll('.data-container')
-const dailyBtn = document.querySelector('#daily_btn')
-const weeklyBtn = document.querySelector('#weekly_btn')
-const monthlyBtn = document.querySelector('#monthly_btn')
-const currentTasks = document.querySelectorAll('.data-container__bottom-area--current')
-const lastTasks = document.querySelectorAll('.data-container__bottom-area--last')
+const profileCard = document.querySelector('.profile') // main card with displayed profile
+const taskCards = document.querySelectorAll('.data-container') // an element of cards with work and time information (animated)
+const dailyBtn = document.querySelector('#daily_btn') // daily button
+const weeklyBtn = document.querySelector('#weekly_btn') // weekly button
+const monthlyBtn = document.querySelector('#monthly_btn') // monthly button
+const currentTasks = document.querySelectorAll('.data-container__bottom-area--current') // current time spend with appropriate selected task
+const lastTasks = document.querySelectorAll('.data-container__bottom-area--last') // last time spend with appropriate selected task
 
 async function worktimeData() {
+	// async function fetching my json file
 	const response = await fetch('./data.json')
 	const data = await response.json()
 
 	function selectBtn(btn) {
+		// function selecting all my time buttons and toggle active class (white color on click)
 		document.querySelectorAll('.timeframe__btn').forEach(button => {
 			button.classList.remove('active')
 		})
@@ -18,6 +20,7 @@ async function worktimeData() {
 	}
 
 	function displayDaily() {
+		// daily display task, displayed as a first view
 		for (let i = 0; i < data.length; i++) {
 			currentTasks[i].innerText = data[i].timeframes.daily.current + 'hrs'
 
@@ -28,38 +31,44 @@ async function worktimeData() {
 	displayDaily()
 
 	dailyBtn.addEventListener('click', e => {
-		// function animateCards(card) {
-		// 	taskCards.forEach(card => {
-		// 		card.style.animation = '1s forwards ease-in cardsMove'
-		// 		card.classList.remove('movecard')
-		// 	})
-		// 	card.classList.add('movecard')
-		// 	card.style.removeProperty('animation')
-		// }
-
 		taskCards.forEach(card => {
-			card.style.removeProperty('animation')
+			// inline adding my animation to my task cards on button click, then remove that style
 			card.style.animation = '1s forwards ease-in cardsMove'
+
+			setTimeout(function () {
+				card.style.removeProperty('animation')
+				card.style.bottom = '0'
+			}, 1000)
 		})
 
 		for (let i = 0; i < data.length; i++) {
-			currentTasks[i].innerText = data[i].timeframes.daily.current + 'hrs'
+			// loop adding data from json file inside my task cards
+			setTimeout(function () {
+				currentTasks[i].innerText = data[i].timeframes.daily.current + 'hrs'
 
-			lastTasks[i].innerText = 'Yesterday - ' + data[i].timeframes.daily.previous + 'hrs'
+				lastTasks[i].innerText = 'Yesterday - ' + data[i].timeframes.daily.previous + 'hrs'
+			}, 500)
 		}
 
-		selectBtn(dailyBtn)
+		selectBtn(dailyBtn) // call selectbtn function (active class add)
 	})
 
 	weeklyBtn.addEventListener('click', e => {
 		taskCards.forEach(card => {
 			card.style.animation = '1s forwards ease-in cardsMove'
+
+			setTimeout(function () {
+				card.style.removeProperty('animation')
+				card.style.bottom = '0'
+			}, 1000)
 		})
 
 		for (let i = 0; i < data.length; i++) {
-			currentTasks[i].innerText = data[i].timeframes.weekly.current + 'hrs'
+			setTimeout(function () {
+				currentTasks[i].innerText = data[i].timeframes.weekly.current + 'hrs'
 
-			lastTasks[i].innerText = 'Last Week - ' + data[i].timeframes.weekly.previous + 'hrs'
+				lastTasks[i].innerText = 'Last Week - ' + data[i].timeframes.weekly.previous + 'hrs'
+			}, 500)
 		}
 
 		selectBtn(weeklyBtn)
@@ -68,20 +77,28 @@ async function worktimeData() {
 	monthlyBtn.addEventListener('click', e => {
 		taskCards.forEach(card => {
 			card.style.animation = '1s forwards ease-in cardsMove'
+
+			setTimeout(function () {
+				card.style.removeProperty('animation')
+				card.style.bottom = '0'
+			}, 1000)
 		})
 
 		for (let i = 0; i < data.length; i++) {
-			currentTasks[i].innerText = data[i].timeframes.monthly.current + 'hrs'
+			setTimeout(function () {
+				currentTasks[i].innerText = data[i].timeframes.monthly.current + 'hrs'
 
-			lastTasks[i].innerText = 'Last Month - ' + data[i].timeframes.monthly.previous + 'hrs'
+				lastTasks[i].innerText = 'Last Month - ' + data[i].timeframes.monthly.previous + 'hrs'
+			}, 500)
 		}
 
 		selectBtn(monthlyBtn)
 	})
 }
-worktimeData()
+worktimeData() // call async function
 
 window.onload = function () {
+	// function starts while page has been loaded
 	profileCard.style.animation = '1.5s 1s forwards ease profileSlideIn'
 
 	taskCards.forEach(card => {
